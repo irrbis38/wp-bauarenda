c6b29a5b2da4b57e15ac43e26c41381dd4b5506c
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-07-26 13:49:21 +0300

    fix: fix page-our-partners

--- template/page-our-partners.php
+++ template/page-our-partners.php
@@ -88,54 +88,26 @@ get_header();
       </div>
     </div>
   </section>
-  <section class="feedback first__feedback">
-    <div class="container">
-      <div class="feedback__wrapper">
-        <h2 class="feedback__title block__title">Обратная связь</h2>
-        <p class="feedback__subtitle">Нет времени выбирать? Заполните заявку и сотрудник вас проконсультирует.</p>
-        <form class="feedback__form" id="second" action="#" novalidate>
-          <p class="error__message">Заполните все обязательные для заполнения поля. Номер телефона должен быть не короче 9 символов.</p>
-          <div class="feedback__item">
-            <input class="feedback__name" type="text" placeholder="Имя*" name="name" minlength="2" required>
-          </div>
-          <div class="feedback__item">
-            <input class="feedback__phone" type="text" placeholder="Телефон*" name="phone" minlength="9" required>
-          </div>
-          <input class="feedback__submit btn__link btn__yellow" id="second_btn" type="submit" value="Отправить" name="submit">
-        </form>
-      </div>
-    </div>
-  </section>
 
   <?php
-  $params = ['posts_per_page' => 4];
-  get_template_part('template-parts/catalog', null, $params);
+  get_template_part('template-parts/feedback-narrow');
+  ?>
+
+  <?php
+  get_template_part('template-parts/popular-part');
   ?>
 
   <?php
   $reviews_params = ['posts_per_page' => 6];
-  get_template_part('template-parts/reviews', null, $reviews_params);
+  get_template_part('template-parts/reviews-part', null, $reviews_params);
   ?>
 
-  <section class="feedback second__feedback">
-    <div class="container">
-      <h2 class="feedback__title block__title">Обратная связь</h2>
-      <p class="feedback__subtitle">Нет времени выбирать? Заполните заявку и сотрудник вас проконсультирует.</p>
-      <form class="feedback__form" action="#" novalidate>
-        <p class="error__message">Заполните все обязательные для заполнения поля. Номер телефона должен быть не короче 9 символов.</p>
-        <div class="feedback__item">
-          <input class="feedback__name" type="text" placeholder="Имя*" name="name" required>
-        </div>
-        <div class="feedback__item">
-          <input class="feedback__phone" type="text" placeholder="Телефон*" name="phone" minlength="9" required>
-        </div>
-        <input class="feedback__submit btn__link btn__yellow" type="submit" value="Отправить" name="submit">
-      </form>
-    </div>
-  </section>
+  <?php
+  get_template_part('template-parts/feedback-wide');
+  ?>
 
   <?php
-  get_template_part('template-parts/fixed');
+  get_template_part('template-parts/fixed-part');
   ?>
 
 </main>
