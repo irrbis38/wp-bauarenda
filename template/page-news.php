cb4aa68c934d24124d08dc38c8b7686cb679610d
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-07-28 13:08:33 +0300

    feat: add page-news

---
+++ template/page-news.php
@@ -0,0 +1,170 @@
+<?php
+/*
+  Template Name: NewsList Template
+*/
+
+get_header();
+?>
+
+
+<main class="main">
+  <section class="news news__page">
+    <div class="container">
+      <div class="first__breadcrumps breadcrumps"><a href="./index.html">Главная</a><span>Новости</span></div>
+      <h2 class="news__title block__title">Новости</h2>
+      <div class="news__wrapper">
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">«Белорусская строительная неделя» снова в Минске</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">Как правильно установить автокран на рабочей строительной площадке</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">Область применения телескопических погрузчиков</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">Весомые причины для аренды специализированной техники</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">Особенности организации вывоза строительного мусора и грунта</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="news__wrapper">
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">«Белорусская строительная неделя» снова в Минске</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">Как правильно установить автокран на рабочей строительной площадке</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">Область применения телескопических погрузчиков</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">Весомые причины для аренды специализированной техники</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+        <div class="news__item element-animation">
+          <h4 class="news__header"><a href="./news-item.html">Особенности организации вывоза строительного мусора и грунта</a></h4>
+          <div class="news__info">
+            <p class="news__published">сегодня 10:00</p>
+            <div class="news__statistics">
+              <div class="news__views"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/view.svg"
+                  alt="количество просмотров"><span>236</span></div>
+              <div class="news__comments"><img class="news__icon" src="<?php echo bloginfo('template_url'); ?>/assets/images/news/comment.svg"
+                  alt="количество просмотров"><span>0</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ul class="page-numbers">
+        <li><span class="page-numbers prev"></span></li>
+        <li><span class="page-numbers current">1</span></li>
+        <li><a class="page-numbers">2</a></li>
+        <li><a class="page-numbers">3</a></li>
+        <li><a class="page-numbers">4</a></li>
+        <li><span class="page-numbers dots">...</span></li>
+        <li><a class="page-numbers">10</a></li>
+        <li><span class="page-numbers next"></span></li>
+      </ul>
+    </div>
+  </section>
+
+
+  <?php
+  $reviews_params = ['posts_per_page' => 6];
+  get_template_part('template-parts/reviews-part', null, $reviews_params);
+  ?>
+
+  <?php
+  get_template_part('template-parts/feedback-wide');
+  ?>
+
+  <?php
+  get_template_part('template-parts/fixed-part');
+  ?>
+</main>
+
+
+<?php
+get_footer();
+?>
