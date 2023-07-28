ba3759a75723ef6311d9b990e51e5220be784352
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-07-28 12:42:17 +0300

    feat: add page-prices

---
+++ template-parts/advantage-hotline-price-part.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template part for advantage-hotline-price-part
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package template
+ */
+
+?>
+
+
+<section class="advantage hotline__advantage prices__advantage">
+  <div class="container">
+    <div class="advantage__container">
+      <div class="advantage__main">
+        <h2 class="advantage__title block__title">Почему стоит обратить внимание именно на нас?</h2>
+        <div class="advantage__text">
+          <p>Самое главное – Вы платите только за фактическое время использования. Час, неделя, месяц – все зависит от реальной необходимости в нашей технике. Это гораздо удобнее и
+            выгоднее, чем содержать собственный парк. </p>
+          <p>Услуги компании охватывают все сферы, в которых может потребоваться трактор, бульдозер, самосвал и т.д. Если Вы занимаетесь строительством или большим ремонтом,
+            отвечаете за уборку или транспортировку – точки соприкосновения обязательно найдутся. Заручитесь поддержкой надежной структуры, чья репутация безупречна. Все вопросы
+            легко уточнить у менеджеров.</p>
+        </div>
+      </div>
+      <div class="advantage__wrapper">
+        <div class="advantage__item">
+          <div class="advantage__icon"><span class="icon svg-image-prices1"></span>
+          </div>
+          <h4 class="advantage__header">Всегда исправная техника;</h4>
+          <p class="advantage__description">У наших техников профильное образование и весомый опыт – техника в порядке.</p>
+        </div>
+        <div class="advantage__border"></div>
+        <div class="advantage__item">
+          <div class="advantage__icon"><span class="icon svg-image-prices2"></span>
+          </div>
+          <h4 class="advantage__header">Мы подбираем модели техники под Ваши нужды</h4>
+          <p class="advantage__description">С нами нет необходимости изучать приспособления и муторно думать.</p>
+        </div>
+        <div class="advantage__item">
+          <div class="advantage__icon"><span class="icon svg-image-prices3"></span>
+          </div>
+          <h4 class="advantage__header">Не нужно думать о месте хранения;</h4>
+          <p class="advantage__description">Нашу технику можно арендовать на пару дней, в промежутках возвращая обратно.</p>
+        </div>
+        <div class="advantage__border"></div>
+        <div class="advantage__item">
+          <div class="advantage__icon"><span class="icon svg-image-prices4"></span>
+          </div>
+          <h4 class="advantage__header">Крупному партнеру – хорошая скидка.</h4>
+          <p class="advantage__description">Если Вы арендуете несколько машин для стройки – Вы получите скидку.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ template/page-prices.php
@@ -0,0 +1,303 @@
+<?php
+/*
+  Template Name: Prices Template
+*/
+
+get_header();
+?>
+
+
+<main class="main">
+  <section class="first prices__page">
+    <div class="container">
+      <div class="first__wrapper">
+        <div class="first__info">
+          <div class="first__breadcrumps breadcrumps"><a href="<?php echo get_home_url(); ?>">Главная</a><span>Цены на аренду строительной техники</span></div>
+          <h1 class="first__title intro__title">Цены на аренду строительной техники</h1>
+          <p class="first__text intro__text">
+            Ценовой фактор при выборе арендодателя спецтехники играет не последнюю роль и часто определяет заказчика в пользу конкретной компании из перечня аналогичных
+            предложений. Мы в «БауАренда» отлично понимаем желание клиентов оптимизировать собственные издержки, поэтому стремимся предложить клиентам выгодные цены.
+
+          </p><a class="first__link btn__link btn__yellow" href="./catalog.html">Каталог</a>
+        </div>
+        <div class="first__img">
+          <picture class="picture object-fit-cover">
+            <source class="object-fit-cover"
+              srcset="<?php echo bloginfo('template_url'); ?>/assets/images/first/first-prices.webp, <?php echo bloginfo('template_url'); ?>/assets/images/first/first-prices@2x.webp 2x"
+              type="image/webp" /><img class="picture__img object-fit-cover" src="<?php echo bloginfo('template_url'); ?>/assets/images/first/first-prices.png"
+              srcset="<?php echo bloginfo('template_url'); ?>/assets/images/first/first-prices.png, images/first/first-prices@2x.png 2x" alt="" />
+          </picture>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="prices features">
+    <div class="container">
+      <h2 class="features__title block__title">Цены</h2>
+      <div class="features__wrapper">
+        <div class="feature__header">
+          <h3>Экскаваторы-погрузчики</h3><span>Цена без НДС</span>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>Venieri VF 10.23C</p>
+            <p>60 BYN</p>
+          </li>
+          <li>
+            <p>JCB 3CX</p>
+            <p>60 BYN</p>
+          </li>
+          <li>
+            <p>JCB 4CX</p>
+            <p>60 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Самосвалы</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>MAN TGS 33.400</p>
+            <p>45 BYN</p>
+          </li>
+          <li>
+            <p>МАЗ</p>
+            <p>45 BYN</p>
+          </li>
+          <li>
+            <p>MАЗ-5516</p>
+            <p>45 BYN</p>
+          </li>
+          <li>
+            <p>MAN TGS 41.390</p>
+            <p>50 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Экскаваторы гусеничные</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>JCB JS 220 болотный</p>
+            <p>60 BYN</p>
+          </li>
+          <li>
+            <p>Hitachi ZX210 LCK-3 полуболотный</p>
+            <p>60 BYN</p>
+          </li>
+          <li>
+            <p>JCB JS 260LC</p>
+            <p>65 BYN</p>
+          </li>
+          <li>
+            <p>Caterpillar 323 DL</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>Сaterpillar 325 DL</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>HITACHI ZX280 LC полуболотный</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>HITACHI ZX 350 H полуболотный</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>Сaterpillar 330BL UHD</p>
+            <p>110 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Бульдозеры</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>KOMATSU D65 EX-12</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>KOMATSU D65 PX-12 болотный</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>KOMATSU D65 PX-12 с рыхлителем</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>KOMATSU D61 PX-12</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>Caterpillar D6R</p>
+            <p>70 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Фронтальные погрузчики</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>АМКОДОР 342С4</p>
+            <p>55 BYN</p>
+          </li>
+          <li>
+            <p>АМКОДОР 352</p>
+            <p>60 BYN</p>
+          </li>
+          <li>
+            <p>АМКОДОР 352</p>
+            <p>55 BYN</p>
+          </li>
+          <li>
+            <p>Venieri VF 18.63 С</p>
+            <p>65 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Колесные экскаваторы</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>HITACHI ZX 170 W-3</p>
+            <p>60 BYN</p>
+          </li>
+          <li>
+            <p>Caterpillar M318D</p>
+            <p>60 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Тралы</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>Низкорамная площадка</p>
+            <p>Договорная</p>
+          </li>
+          <li>
+            <p>Полуприцеп-тяжеловоз (трал) Steelbear TR-31</p>
+            <p>Договорная</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Грунтовые катки</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>Bomag BW-211 D-4</p>
+            <p>65 BYN</p>
+          </li>
+          <li>
+            <p>Hamm 3520</p>
+            <p>70 BYN</p>
+          </li>
+          <li>
+            <p>Hamm 3625</p>
+            <p>75 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Автокраны</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>Hamm Zoomlion QY90V3625</p>
+            <p>180-230 BYN</p>
+          </li>
+          <li>
+            <p>Liebherr LTM 1160</p>
+            <p>280-320 BYN</p>
+          </li>
+          <li>
+            <p>Grove GMK 6300</p>
+            <p>550 BYN</p>
+          </li>
+          <li>
+            <p>Liebherr LTM 1500</p>
+            <p>1900 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Телескопические погрузчики</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>Manitou MT1740</p>
+            <p>65 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Автогрейдеры</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>ГС 14.02 (ДЗ-180)</p>
+            <p>70 BYN</p>
+          </li>
+        </ul>
+        <div class="feature__header">
+          <h3>Мини-техника</h3>
+        </div>
+        <ul class="feature__list">
+          <li>
+            <p>Экскаватор-погрузчик Venieri VF 1.33B</p>
+            <p>45 BYN</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <?php
+  get_template_part('template-parts/feedback-narrow');
+  ?>
+
+  <?php
+  get_template_part('template-parts/advantage-hotline-price-part');
+  ?>
+
+  <section class="infoBlock prices__infoBlock">
+    <div class="container">
+      <h2 class="infoBlock__title block__title">Работа с нами</h2>
+      <div class="infoBlock__wrapper">
+        <p>Поскольку мы работаем с собственной спецтехникой, то можем предложить клиентам не просто демократичные цены, но и комплекс дополнительных услуг. Мы предоставляем технику
+          в аренду уже с экипажем, а также полным комплектом необходимого для работы навесного оборудования и горюче-смазочных материалов.</p>
+        <p>Кроме этого, нашим клиентам доступен комплекс дополнительных выгод:</p>
+        <ul>
+          <li>скидки при заказе 2 и более единиц спецтехники заказчик;</li>
+          <li>сниженная почасовая ставка при длительном сроке аренды;</li>
+          <li>возможность выбрать почасовую или посменную схему оплаты;</li>
+          <li>сроки аренды могут варьироваться от нескольких часов до нескольких лет.</li>
+        </ul>
+        <p>Напоминаем, что на этой странице указана примерная стоимость почасовой аренды техники. Окончательная цифра рассчитывается отдельно для каждого заказчика, исходя из
+          количества единиц заказанной техники, стоимости доставки техники на объект клиента, продолжительности срока аренды. Цена фиксируется в официальном договоре, а услуги
+          компании оплачиваются исключительно в безналичном формате.</p>
+        <p>Работать с «БауАренда» удобно и выгодно. Попробуйте и убедитесь в этом сами.</p>
+        <p><b>* Внимание! Цены представлены за арендную ставку 1 маш-час с ГСМ и экипажем</b></p>
+        <p><b>* Доставка техники осуществляется за счет арендатора</b></p>
+      </div>
+    </div>
+  </section>
+
+
+  <?php
+  $reviews_params = ['posts_per_page' => 6];
+  get_template_part('template-parts/reviews-part', null, $reviews_params);
+  ?>
+
+  <?php
+  get_template_part('template-parts/feedback-wide');
+  ?>
+
+  <?php
+  get_template_part('template-parts/fixed-part');
+  ?>
+</main>
+
+
+<?php
+get_footer();
+?>
